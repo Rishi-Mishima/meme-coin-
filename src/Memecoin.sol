f3d2336c6a61987a451390d7dadab0227924a47d
Your Name <153814332+Rishi-Mishima@users.noreply.github.com>
2026-08-30 16:46:17 +0200

    完成了transfer测试

--- src/Memecoin.sol
+++ src/Memecoin.sol
@@ -2,20 +2,20 @@
 
 pragma solidity ^0.8.24;
 
-contract Memecoin{
+contract MemeCoin{
     string public name = "Miyuki meme coin"; 
     string public symbol = "MIYU"; 
 
     uint256 public totalSupply = 1_000_000;
 
-    mapping (addres => uint256) public balances; 
+    mapping (address => uint256) public balances; 
 
     constructor() {
         balances[msg.sender] = totalSupply;
     }
 
     function transfer(address to, uint256 amount) public {
-        require(balances(msg.sender) >= amount, "NOT ENOUGHT AMOUNT");
+       require(balances[msg.sender] >= amount, "NOT ENOUGH AMOUNT");
 
         balances[msg.sender] -= amount;
         balances[to] += amount;
--- test/MemeCoin.t.sol
+++ test/MemeCoin.t.sol
@@ -2,6 +2,27 @@
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
-import {Memecoin} from "../src/MemeCoin.sol";
+import {MemeCoin} from "../src/Memecoin.sol";
 
-contract 
+contract MemeCoinTest is Test{
+    MemeCoin token; 
+
+    address alice = address(0xA11CE);
+    address bob = address(0xB0B);
+
+    function setUp() public {
+        token = new MemeCoin(); 
+    }
+
+    function testInitiaSupplyBelongsToDeployer() public {
+        assertEq (token.balances(address(this)),1_000_000 );   
+    }
+
+    function testTransfer() public {
+        token.transfer(alice, 100);
+
+        assertEq(token.balances(alice), 100);
+        assertEq(token.balances(address(this)), 999_900);
+    }
+
+}
