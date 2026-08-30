216bd2d4b97d02a5df5e5dbbd57c41204f7d1f36
Your Name <153814332+Rishi-Mishima@users.noreply.github.com>
2026-08-30 17:47:47 +0200

    完成ERC和test

--- src/Memecoin.sol
+++ src/Memecoin.sol
@@ -3,21 +3,35 @@
 pragma solidity ^0.8.24;
 
 contract MemeCoin {
+    // 写出ERC声明
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
     string public name = "Miyuki meme coin";
     string public symbol = "MIYU";
 
     uint256 public totalSupply = 1_000_000;
 
-    mapping(address => uint256) public balances;
+    mapping(address => uint256) private _balances;
 
     constructor() {
-        balances[msg.sender] = totalSupply;
+        _balances[msg.sender] = totalSupply;
     }
 
-    function transfer(address to, uint256 amount) public {
-        require(balances[msg.sender] >= amount, "NOT ENOUGH AMOUNT");
+    // 实现ERC-20标准函数
+    // account 是address里有多少钱, amount是转的钱
+    function balancesOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
 
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
+    function transfer(address to, uint256 amount) public returns (bool) {
+        require(_balances[msg.sender] >= amount, "NOT ENOUGH AMOUNT");
+
+        _balances[msg.sender] -= amount;
+        _balances[to] += amount;
+
+        // 记录一条链上日志：谁给谁转了多少币。
+        emit Transfer(msg.sender, to, amount);
+
+        return true;
     }
 }
--- test/MemeCoin.t.sol
+++ test/MemeCoin.t.sol
@@ -5,6 +5,8 @@ import {Test} from "forge-std/Test.sol";
 import {MemeCoin} from "../src/Memecoin.sol";
 
 contract MemeCoinTest is Test {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
     MemeCoin token;
 
     address alice = address(0xA11CE);
@@ -15,26 +17,39 @@ contract MemeCoinTest is Test {
     }
 
     function testInitiaSupplyBelongsToDeployer() public {
-        assertEq(token.balances(address(this)), 1_000_000);
+        assertEq(token.balancesOf(address(this)), 1_000_000);
     }
 
     function testTransfer() public {
         token.transfer(alice, 100);
 
-        assertEq(token.balances(alice), 100);
-        assertEq(token.balances(address(this)), 999_900);
+        assertEq(token.balancesOf(alice), 100);
+        assertEq(token.balancesOf(address(this)), 999_900);
     }
 
     function testTransferFailsWhenBalanceNotEnough() public {
-        uint256 senderBalanceBefore = token.balances(address(this));
-        uint256 bobBalanceBefore = token.balances(bob);
+        uint256 senderBalanceBefore = token.balancesOf(address(this));
+        uint256 bobBalanceBefore = token.balancesOf(bob);
 
         vm.expectRevert("NOT ENOUGH AMOUNT");
 
         token.transfer(bob, senderBalanceBefore + 1);
 
-        assertEq(token.balances(address(this)), senderBalanceBefore);
+        assertEq(token.balancesOf(address(this)), senderBalanceBefore);
 
-        assertEq(token.balances(bob), bobBalanceBefore);
+        assertEq(token.balancesOf(bob), bobBalanceBefore);
+    }
+
+    function testTransferReturnsTrue() public {
+        bool success = token.transfer(bob, 100);
+        assertTrue(success);
+    }
+
+    function testTransferEmitsEvent() public {
+        vm.expectEmit(true, true, false, true);
+
+        emit Transfer(address(this), bob, 100);
+
+        token.transfer(bob, 100);
     }
 }
